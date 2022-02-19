25e3aa848d10fff27f7ebae3eb671d75c63ab41c
pulzed <danijel.durakovic@gmail.com>
2022-02-19 23:37:04 +0100

    Init dev branch

--- util/update_versions.py
+++ util/update_versions.py
@@ -3,22 +3,26 @@ import fileinput
 import sys
 
 if len(sys.argv) < 2:
-    sys.stdout.write('Specify version!\n')
-    sys.exit(0)
+	sys.stdout.write('Specify version!\n')
+	sys.exit(0)
 
 substituteVerString = sys.argv[1]
 
 sources = [
-    '../src/app.cpp',
-    '../src/app.h'
+	'../src/about.cpp',
+	'../src/about.h',
+	'../src/app.cpp',
+	'../src/app.h',
+	'../src/config.cpp',
+	'../src/config.h'
 ]
 
 if __name__ == '__main__':
-    for sourcefile in sources:
-        for i, line in enumerate(fileinput.input(sourcefile, inplace=True)):
-            if i == 3:
-                nextline = '//  |    -,  | | | |  |   __|  _  |  version ' + substituteVerString
-                print(nextline)
-            else:
-                print(line, end='')
-        sys.stdout.write('Successfully written ' + sourcefile + '\n')
+	for sourcefile in sources:
+		for i, line in enumerate(fileinput.input(sourcefile, inplace=True)):
+			if i == 3:
+				nextline = '//  |    -,  | | | |  |   __|  _  |  version ' + substituteVerString
+				print(nextline)
+			else:
+				print(line, end='')
+		sys.stdout.write('Successfully written ' + sourcefile + '\n')
