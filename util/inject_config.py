78609ed91cf95b07a393e7e24b36d300f0f86f5c
pulzed <danijel.durakovic@gmail.com>
2022-04-06 00:47:30 +0200

    Fix header in util

--- util/inject_config.py
+++ util/inject_config.py
@@ -1,4 +1,4 @@
-# This util injects data from dev/config.ini into src/default_config.cpp.
+# This util injects data from dev/config.ini into src/const.cpp.
 #
 # WARNING Always run on a clean working tree, then diff and make sure everything is
 # in order before commiting to branch!
